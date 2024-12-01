43503dae075462cd36c03ba6d3cb2e259a06beec
eric damian <ericdamian@MacBook-Pro-de-eric.local>
2024-11-30 22:17:17 -0300

    implementado toptab exitoso

--- App.tsx
+++ App.tsx
@@ -1,36 +1,14 @@
-import './gesture-handler';
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from './src/presentation/screens/login/Login';
-import Home from './src/presentation/screens/home/Home';
-import Archivos from './src/presentation/screens/archivos/Archivos';
-import Nuevo from './src/presentation/screens/archivos/Nuevo';
-import FormularioModal from './src/presentation/screens/login/FormularioModal';
 import NavegacionStack from './src/presentation/routes/NavegacionStack';
-import NavegacionTop from './src/presentation/routes/NavegacionTop';
-import { SafeAreaView } from 'react-native';
-
-
 
 const App = () => {
-
-
   return (
-    
     <NavigationContainer>
-      
-
-      <NavegacionTop />
-      
-      
-      
-      </NavigationContainer>
-      
-    
+      <NavegacionStack />
+    </NavigationContainer>
   );
 };
 
-
-
 export default App;
+
--- src/presentation/routes/NavegacionStack.tsx
+++ src/presentation/routes/NavegacionStack.tsx
@@ -1,41 +1,37 @@
-import React from 'react'
-import { SafeAreaView, SafeAreaViewBase, Text, View } from 'react-native'
-
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import NavegacionTop from './NavegacionTop';
 import Archivos from '../screens/archivos/Archivos';
 import Archivo from '../screens/archivos/Archivo';
 import Nuevo from '../screens/archivos/Nuevo';
 
-import NavegacionTop from './NavegacionTop';
-
 const Stack = createStackNavigator();
 
 const NavegacionStack = () => {
+  return (
+    <Stack.Navigator initialRouteName="NavegacionTop">
+      <Stack.Screen 
+        name="NavegacionTop" 
+        component={NavegacionTop} 
+        options={{ title: 'INICIO' }} 
+      />
+      <Stack.Screen 
+        name="Archivos" 
+        component={Archivos} 
+        options={{ title: 'Historial' }} 
+      />
+      <Stack.Screen 
+        name="Archivo" 
+        component={Archivo} 
+        options={{ title: 'Archivo' }} 
+      />
+      <Stack.Screen 
+        name="Nuevo" 
+        component={Nuevo} 
+        options={{ title: 'Nuevo' }} 
+      />
+    </Stack.Navigator>
+  );
+};
 
-    return (
-        
-
-        
-
-        <Stack.Navigator
-
-        >
-
-            <Stack.Screen name="NavegacionTop" component={NavegacionTop}
-                options={{ title: 'INICIO' }} />
-            <Stack.Screen name="Archivos" component={Archivos}
-                options={{ title: 'Historial' }} />
-            <Stack.Screen name="Archivo" component={Archivo}
-                options={{ title: 'Archivo' }} />
-            <Stack.Screen name="Nuevo" component={Nuevo}
-                options={{ title: 'Nuevo' }} />
-
-
-        </Stack.Navigator>
-       
-
-    );
-}
-
-export default NavegacionStack
+export default NavegacionStack;
